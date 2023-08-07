93255c5b12abb438f6ea335beaf91d85492d0104
Yatin Gupta <guptayatin4@gmail.com>
2023-08-06 23:23:17 -0500

    Finished Box component for sidebar, working on mapping the routes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -42,7 +43,25 @@ const Sidebar: React.FC<SidebarProps> = ({
                 "
             >
                 <Box>
-                    Sidebar Navigation
+                    <div
+                        className="
+                            flex
+                            flex-col
+                            gap-y-4
+                            px-5
+                            py-4
+                        "
+                    >
+                        {routes.map((item) => (
+                            <SidebarItem
+                                key={item.label}
+                                {...item}
+                            />
+                        ))}
+                    </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    Song Library
                 </Box>
             </div>
         </div>

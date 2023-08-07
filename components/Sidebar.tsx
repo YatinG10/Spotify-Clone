d40d9aa66dbeb63a4863d29bdb905b2982addc60
Yatin Gupta <guptayatin4@gmail.com>
2023-08-06 23:14:12 -0500

    added routes for sidebar and icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+
 interface SidebarProps {
     children: React.ReactNode;
 }
@@ -7,9 +12,39 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({
     children
 }) => {
+    const pathname = usePathname;
+    const routes = useMemo(() => [
+        {
+            icon: HiHome,
+            label: 'Home',
+            active: pathname !== '/search',
+            href: '/',
+        },
+        {
+            icon: BiSearch,
+            label: 'Search',
+            active: pathname === '/search',
+            href: '/search',
+        }
+    ], [pathname]);
     return ( 
-        <div>
-            {children}
+        <div className="flex h-full">
+            <div
+                className="
+                    hidden
+                    md:flex
+                    flex-col
+                    gap-y-2
+                    bg-black
+                    h-full
+                    w-[300px]
+                    p-2
+                "
+            >
+                <Box>
+                    Sidebar Navigation
+                </Box>
+            </div>
         </div>
     );
 }
